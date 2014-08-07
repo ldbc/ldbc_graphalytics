801f2f52f58ea5b7a4ca4c64fab2b45f53c10837
Tim Hegeman <tim.m.hegeman@gmail.com>
2014-08-07 16:54:40 +0200

    Set up YARN project

---
+++ platforms/yarn/src/main/java/org/tudelft/graphalytics/yarn/YarnPlatform.java
@@ -0,0 +1,24 @@
+package org.tudelft.graphalytics.yarn;
+
+import org.tudelft.graphalytics.AlgorithmType;
+import org.tudelft.graphalytics.Platform;
+
+public class YarnPlatform implements Platform {
+
+	public void uploadGraph(String graphName, String graphFilePath) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void executeAlgorithmOnGraph(AlgorithmType algorithmType,
+			String graphName) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void deleteGraph(String graphName) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
