8b78b5bf90b024cc64801cfc606c6b39a4448083
Tim Hegeman <tim.m.hegeman@gmail.com>
2014-11-30 20:16:32 +0100

    Updated launch scripts to be configurable

--- run-benchmark.sh
+++ run-benchmark.sh
@@ -2,25 +2,68 @@
 
 set -e
 
-if [ "$#" -ge "1" ] && [ "$1" = "-compile" ]; then
-	shift
-	DO_COMPILE=true
-fi
+rootdir=$(dirname $(readlink -f ${BASH_SOURCE[0]}))
+config="${rootdir}/config/"
 
+function print-usage() {
+	echo "Usage: ${BASH_SOURCE[0]} [--config <dir>] [--compile] <platform>" >&2
+}
+
+# Parse the command-line arguments
+while :
+do
+	case "$1" in
+		--config)                      # Use a different config directory
+			config="$(readlink -f \"$2\")"
+			shift 2
+			;;
+		--compile)                     # Compile before running the benchmark
+			compile="compile"
+			shift
+			;;
+		--)                            # End of options
+			shift
+			break
+			;;
+		-*)                            # Unknown command line option
+			echo "Unknown option: $1" >&2
+			print-usage
+			exit 1
+			;;
+		*)                             # End of options
+			break
+			;;
+	esac
+done
+
+# Ensure the mandatory arguments are present
 if [ "$#" -lt "1" ]; then
-	echo "Usage: ${BASH_SOURCE[0]} [-compile] <platform>"
+	print-usage
+	exit 1
+fi
+platform="$1"
+
+# Change directory to the graphalytics project root
+cd $rootdir
+
+# Verify that the project exists
+if [ ! -d "platforms/$platform" ]; then
+	echo "Unknown platform: $platform" >&2
+	print-usage
 	exit 1
 fi
 
-PLATFORM=$1
-
-cd $(dirname ${BASH_SOURCE[0]})
-
-if [ "$DO_COMPILE" = "true" ]; then
-	. ./compile-benchmark.sh $PLATFORM
+# Compile the project if requested
+if [ "$compile" = "compile" ]; then
+	echo "Compiling $platform"
+	. compile-benchmark.sh $platform
 fi
 
-CLASSPATH=$(find $(pwd)/platforms/$PLATFORM/target/graphalytics-platforms-*-jar-with-dependencies.jar):$(platforms/$PLATFORM/compute-classpath.sh):$(pwd)/config/
-export CLASSPATH=$CLASSPATH
+# Execute platform specific initialization
+export config=$config
+. platforms/$platform/prepare-benchmark.sh "$@"
 
-java -cp $CLASSPATH org.tudelft.graphalytics.Graphalytics $PLATFORM
+# Run the benchmark
+export CLASSPATH=$config:$(find $(pwd)/platforms/$platform/target/graphalytics-platforms-*-jar-with-dependencies.jar):$platform_classpath
+java -cp $CLASSPATH org.tudelft.graphalytics.Graphalytics $platform $platform_opts
+
