9cfe0e3c2a15554becc5607902ca0da8e186793a
Tim Hegeman <tim.m.hegeman@gmail.com>
2015-01-05 17:07:26 +0100

    Finalized the GraphX implementation

--- platforms/graphx/src/main/scala/org/tudelft/graphalytics/graphx/GraphLoader.scala
+++ platforms/graphx/src/main/scala/org/tudelft/graphalytics/graphx/GraphLoader.scala
@@ -5,21 +5,40 @@ import org.apache.spark.graphx.{VertexId, Graph}
 import org.tudelft.graphalytics.GraphFormat
 import scala.reflect.ClassTag
 
+/**
+ * Utility class for loading graphs in various formats (directed vs undirected, vertex- vs edge-based).
+ */
 object GraphLoader {
 
+	/**
+	 * @param graphData raw graph data represent as one string per line of input
+	 * @param graphFormat the graph data format specification
+	 * @param defaultValue default value for each vertex
+	 * @tparam VD vertex value type
+	 * @return a parsed GraphX graph
+	 */
 	def loadGraph[VD : ClassTag](graphData : RDD[String], graphFormat : GraphFormat,
 			defaultValue : VD) : Graph[VD, Int] =
 		Graph.fromEdgeTuples(loadEdges(graphFormat)(graphData), defaultValue)
-	
+
+	/**
+	 * Convenience method for selected the correct load function based on the graph format.
+	 *
+	 * @param graphFormat the graph format specification
+	 * @return a function that can parse edges from the given input specification
+	 */
 	private def loadEdges(graphFormat : GraphFormat) : RDD[String] => RDD[(VertexId, VertexId)] = {
 		(graphFormat.isEdgeBased(), graphFormat.isDirected()) match {
-			case (false, false) => loadEdgesFromVertexData
-			case (false, true) => loadEdgesFromVertexData
+			case (false, _) => loadEdgesFromVertexData
 			case (true, true) => loadEdgesFromEdgeDirectedData
 			case (true, false) => loadEdgesFromEdgeUndirectedData
 		}
 	}
-	
+
+	/**
+	 * @param graphData graph data in directed, edge-based format
+	 * @return a set of parsed edges
+	 */
 	private def loadEdgesFromEdgeDirectedData(graphData : RDD[String]) : RDD[(VertexId, VertexId)] = {
 		def lineToEdge(s : String) : (VertexId, VertexId) = {
 			val tokens = s.trim.split("""\s""")
@@ -28,7 +47,11 @@ object GraphLoader {
 		
 		graphData.map(lineToEdge)
 	}
-	
+
+	/**
+	 * @param graphData graph data in undirected, edge-based format
+	 * @return a set of parsed edges
+	 */
 	private def loadEdgesFromEdgeUndirectedData(graphData : RDD[String]) : RDD[(VertexId, VertexId)] = {
 		def lineToEdges(s : String) : Array[(VertexId, VertexId)] = {
 			val tokens = s.trim.split("""\s""")
@@ -37,7 +60,11 @@ object GraphLoader {
 		
 		graphData.flatMap(lineToEdges)
 	}
-	
+
+	/**
+	 * @param graphData graph data in vertex-based format
+	 * @return a set of parsed edges
+	 */
 	private def loadEdgesFromVertexData(graphData : RDD[String]) : RDD[(VertexId, VertexId)] = {
 		def lineToEdges(s : String) : Array[(VertexId, VertexId)] = {
 			val tokens = s.trim.split("""\s""")
--- platforms/graphx/src/main/scala/org/tudelft/graphalytics/graphx/GraphXPlatform.scala
+++ platforms/graphx/src/main/scala/org/tudelft/graphalytics/graphx/GraphXPlatform.scala
@@ -12,26 +12,50 @@ import org.tudelft.graphalytics.graphx.conn.ConnectedComponentsJob
 import org.tudelft.graphalytics.graphx.evo.ForestFireModelJob
 import org.tudelft.graphalytics.graphx.stats.LocalClusteringCoefficientJob
 
+/**
+ * Constants for GraphXPlatform
+ */
 object GraphXPlatform {
 	val HDFS_PATH = "graphalytics-graphx"
+
+	val CONFIG_PATH = "graphx.properties"
+	val CONFIG_JOB_NUM_EXECUTORS = "graphx.job.num-executors"
+	val CONFIG_JOB_EXECUTOR_MEMORY = "graphx.job.executor-memory"
+	val CONFIG_JOB_EXECUTOR_CORES = "graphx.job.executor-cores"
 }
 
+/**
+ * Graphalytics Platform implementation for GraphX. Manages the datasets on HDFS and launches the appropriate
+ * GraphX jobs.
+ */
 class GraphXPlatform extends Platform {
 	import GraphXPlatform._
-	
+
 	var pathsOfGraphs : Map[String, String] = Map()
-	
+
+	/* Parse the GraphX configuration file */
+	val config = Properties.fromFile(CONFIG_PATH).getOrElse(Properties.empty())
+	System.setProperty("spark.executor.cores", config.getString(CONFIG_JOB_EXECUTOR_CORES).getOrElse("1"))
+	System.setProperty("spark.executor.memory", config.getString(CONFIG_JOB_EXECUTOR_MEMORY).getOrElse("2g"))
+	System.setProperty("spark.executor.instances", config.getString(CONFIG_JOB_NUM_EXECUTORS).getOrElse("1"))
+
+	/**
+	 * @inheritdoc
+	 */
 	def uploadGraph(graph : Graph, filePath : String) = {
 		val localPath = new Path(filePath)
 		val hdfsPath = new Path(s"$HDFS_PATH/input/${graph.getName}")
-		
+
 		val fs = FileSystem.get(new Configuration())
 		fs.copyFromLocalFile(localPath, hdfsPath)
-		fs.close
+		fs.close()
 		
 		pathsOfGraphs += (graph.getName -> hdfsPath.toUri.getPath)
 	}
 
+	/**
+	 * @inheritdoc
+	 */
 	def executeAlgorithmOnGraph(algorithmType : AlgorithmType,
 			graph : Graph, parameters : Object) : Boolean = {
 		try  {
@@ -62,10 +86,16 @@ class GraphXPlatform extends Platform {
 		}
 	}
 
+	/**
+	 * @inheritdoc
+	 */
 	def deleteGraph(graphName : String) = {
 		// Not implemented
 	}
 
+	/**
+	 * @inheritdoc
+	 */
 	def getName() : String = "graphx"
 
 }
