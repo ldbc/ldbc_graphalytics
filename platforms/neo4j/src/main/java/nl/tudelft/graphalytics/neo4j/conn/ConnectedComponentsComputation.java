2944b8ea2236b649377fc40da7c5295e5be4adfe
Tim Hegeman <tim.m.hegeman@gmail.com>
2015-02-01 20:20:55 +0100

    Implemented connected components on Neo4j

--- platforms/neo4j/src/main/java/nl/tudelft/graphalytics/neo4j/conn/ConnectedComponentsComputation.java
+++ platforms/neo4j/src/main/java/nl/tudelft/graphalytics/neo4j/conn/ConnectedComponentsComputation.java
@@ -1,6 +1,8 @@
 package nl.tudelft.graphalytics.neo4j.conn;
 
-import org.neo4j.graphdb.GraphDatabaseService;
+import nl.tudelft.graphalytics.neo4j.Neo4jConfiguration;
+import org.neo4j.graphdb.*;
+import org.neo4j.tooling.GlobalGraphOperations;
 
 /**
  * Implementation of the connected components algorithm in Neo4j. This class is responsible for the computation,
@@ -25,7 +27,43 @@ public class ConnectedComponentsComputation {
 	 * ID in each component.
 	 */
 	public void run() {
+		// Initialize the component property of all nodes to their own ID
+		initializeComponents();
 
+		// Repeatedly assign to each node the minimum component IDs of their neighbours, until convergence
+		boolean finished = false;
+		while (!finished) {
+			finished = true;
+			try (Transaction transaction = graphDatabase.beginTx()) {
+				for (Node node : GlobalGraphOperations.at(graphDatabase).getAllNodes()) {
+					long componentId = getComponentId(node);
+					if (componentId != (long) node.getProperty(COMPONENT)) {
+						node.setProperty(COMPONENT, componentId);
+						finished = false;
+					}
+				}
+				transaction.success();
+			}
+		}
+	}
+
+	private void initializeComponents() {
+		try (Transaction transaction = graphDatabase.beginTx()) {
+			for (Node node : GlobalGraphOperations.at(graphDatabase).getAllNodes()) {
+				node.setProperty(COMPONENT, node.getProperty(Neo4jConfiguration.ID_PROPERTY));
+			}
+			transaction.success();
+		}
+	}
+
+	private long getComponentId(Node node) {
+		long smallestComponentId = (long) node.getProperty(COMPONENT);
+		for (Relationship edge : node.getRelationships(Neo4jConfiguration.EDGE, Direction.BOTH)) {
+			long componentId = (long) edge.getOtherNode(node).getProperty(COMPONENT);
+			if (componentId < smallestComponentId)
+				smallestComponentId = componentId;
+		}
+		return smallestComponentId;
 	}
 
 }
