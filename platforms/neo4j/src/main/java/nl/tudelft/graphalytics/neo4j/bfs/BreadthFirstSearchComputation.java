68f946b3b4c58df0b6b29216da3e0f966201acc5
Tim Hegeman <tim.m.hegeman@gmail.com>
2015-02-01 17:29:02 +0100

    Added implementation of BFS computation on Neo4j

--- platforms/neo4j/src/main/java/nl/tudelft/graphalytics/neo4j/bfs/BreadthFirstSearchComputation.java
+++ platforms/neo4j/src/main/java/nl/tudelft/graphalytics/neo4j/bfs/BreadthFirstSearchComputation.java
@@ -1,6 +1,10 @@
 package nl.tudelft.graphalytics.neo4j.bfs;
 
-import org.neo4j.graphdb.GraphDatabaseService;
+import nl.tudelft.graphalytics.neo4j.Neo4jConfiguration;
+import org.neo4j.graphdb.*;
+import org.neo4j.graphdb.traversal.Evaluators;
+import org.neo4j.graphdb.traversal.TraversalDescription;
+import org.neo4j.graphdb.traversal.Traverser;
 
 /**
  * Implementation of the breadth-first search algorithm in Neo4j. This class is responsible for the computation of the
@@ -21,7 +25,19 @@ public class BreadthFirstSearchComputation {
 	}
 
 	public void run() {
+		try (Transaction transaction = graphDatabase.beginTx()) {
+			Node startNode = graphDatabase.getNodeById(startVertexId);
+			TraversalDescription traversalDescription = graphDatabase.traversalDescription()
+					.breadthFirst()
+					.relationships(Neo4jConfiguration.EDGE, Direction.OUTGOING)
+					.evaluator(Evaluators.all());
 
+			Traverser traverser = traversalDescription.traverse(startNode);
+			for (Path path : traverser) {
+				path.endNode().setProperty(DISTANCE, (long)path.length());
+			}
+			transaction.success();
+		}
 	}
 
 }
