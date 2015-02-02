f83b69dfbe0bfd82f312f9c4bb463cd69078c613
Tim Hegeman <tim.m.hegeman@gmail.com>
2015-02-02 01:00:36 +0100

    Implemented community detection computation

--- platforms/neo4j/src/main/java/nl/tudelft/graphalytics/neo4j/cd/CommunityDetectionComputation.java
+++ platforms/neo4j/src/main/java/nl/tudelft/graphalytics/neo4j/cd/CommunityDetectionComputation.java
@@ -1,6 +1,11 @@
 package nl.tudelft.graphalytics.neo4j.cd;
 
-import org.neo4j.graphdb.GraphDatabaseService;
+import nl.tudelft.graphalytics.neo4j.Neo4jConfiguration;
+import org.neo4j.graphdb.*;
+import org.neo4j.tooling.GlobalGraphOperations;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Implementation of the community detection algorithm in Neo4j. This class is responsible for the computation,
@@ -11,6 +16,10 @@ import org.neo4j.graphdb.GraphDatabaseService;
 public class CommunityDetectionComputation {
 
 	public static final String LABEL = "LABEL";
+	public static final String LABEL_SCORE = "LABEL_SCORE";
+	public static final String NEW_LABEL = "NEW_LABEL";
+	public static final String NEW_LABEL_SCORE = "NEW_LABEL_SCORE";
+
 	private final GraphDatabaseService graphDatabase;
 	private final float nodePreference;
 	private final float hopAttenuation;
@@ -35,7 +44,88 @@ public class CommunityDetectionComputation {
 	 * community to which the node belongs.
 	 */
 	public void run() {
+		// Initialize the label of each node to its own ID
+		initializeLabels();
 
+		int iteration = 0;
+		boolean converged = false;
+		while (!converged && iteration < maxIterations) {
+			converged = true;
+			try (Transaction transaction = graphDatabase.beginTx()) {
+				for (Node node : GlobalGraphOperations.at(graphDatabase).getAllNodes()) {
+					computeNewLabel(node);
+				}
+				for (Node node : GlobalGraphOperations.at(graphDatabase).getAllNodes()) {
+					converged = converged & updateLabel(node);
+				}
+				transaction.success();
+			}
+			iteration++;
+		}
+	}
+
+	private void initializeLabels() {
+		try (Transaction transaction = graphDatabase.beginTx()) {
+			for (Node node : GlobalGraphOperations.at(graphDatabase).getAllNodes()) {
+				node.setProperty(LABEL, node.getProperty(Neo4jConfiguration.ID_PROPERTY));
+				node.setProperty(LABEL_SCORE, 1.0);
+			}
+			transaction.success();
+		}
+	}
+
+	private void computeNewLabel(Node node) {
+		Map<Long, Double> labelScores = new HashMap<>();
+		Map<Long, Double> maxLabelScores = new HashMap<>();
+		for (Relationship edge : node.getRelationships(Neo4jConfiguration.EDGE, Direction.BOTH)) {
+			Node other = edge.getOtherNode(node);
+			long label = (long) other.getProperty(LABEL);
+			int degree = other.getDegree(Neo4jConfiguration.EDGE, Direction.BOTH);
+			double score = (double) other.getProperty(LABEL_SCORE);
+			double weighedScore = score * Math.pow(degree, nodePreference);
+
+			if (!labelScores.containsKey(label)) {
+				labelScores.put(label, weighedScore);
+				maxLabelScores.put(label, score);
+			} else {
+				labelScores.put(label, weighedScore + labelScores.get(label));
+				if (score > maxLabelScores.get(label))
+					maxLabelScores.put(label, score);
+			}
+		}
+
+		long maxLabel = 0;
+		double maxLabelScore = Double.NEGATIVE_INFINITY;
+		for (long label : labelScores.keySet()) {
+			if (labelScores.get(label) > maxLabelScore) {
+				maxLabel = label;
+				maxLabelScore = labelScores.get(label);
+			} else if (labelScores.get(label) == maxLabelScore && label < maxLabel) {
+				maxLabel = label;
+			}
+		}
+
+		maxLabelScore = maxLabelScores.get(maxLabel);
+		if (maxLabel != (long) node.getProperty(LABEL)) {
+			maxLabelScore -= hopAttenuation;
+		}
+
+		node.setProperty(NEW_LABEL, maxLabel);
+		node.setProperty(NEW_LABEL_SCORE, maxLabelScore);
+	}
+
+	private boolean updateLabel(Node node) {
+		if ((long) node.getProperty(LABEL) == (long) node.getProperty(NEW_LABEL)) {
+			node.removeProperty(NEW_LABEL);
+			node.removeProperty(NEW_LABEL_SCORE);
+			return true;
+		} else {
+			node.setProperty(LABEL, node.getProperty(NEW_LABEL));
+			node.setProperty(LABEL_SCORE, node.getProperty(NEW_LABEL_SCORE));
+			node.removeProperty(NEW_LABEL);
+			node.removeProperty(NEW_LABEL_SCORE);
+			return false;
+		}
 	}
 
 }
--- platforms/neo4j/src/test/java/nl/tudelft/graphalytics/neo4j/cd/CommunityDetectionComputationTest.java
+++ platforms/neo4j/src/test/java/nl/tudelft/graphalytics/neo4j/cd/CommunityDetectionComputationTest.java
@@ -1,7 +1,6 @@
 package nl.tudelft.graphalytics.neo4j.cd;
 
 import nl.tudelft.graphalytics.neo4j.AbstractComputationTest;
-import org.junit.Assert;
 import org.junit.Test;
 import org.neo4j.graphdb.Transaction;
 
@@ -9,10 +8,15 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import static nl.tudelft.graphalytics.neo4j.cd.CommunityDetectionComputation.LABEL;
 import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.Matchers.isIn;
+import static org.hamcrest.Matchers.not;
+import static org.junit.Assert.assertThat;
 
 /**
  * Test case for the community detection implementation on Neo4j.
@@ -24,28 +28,6 @@ public class CommunityDetectionComputationTest extends AbstractComputationTest {
 	private static final float NODE_PREFERENCE = 0.1f;
 	private static final float HOP_ATTENUATION = 0.1f;
 	private static final int MAX_ITERATIONS = 5;
-
-	@Test
-	public void testExample() throws IOException {
-		// Load data
-		loadGraphFromResource("/test-examples/cd-input");
-		// Execute algorithm
-		new CommunityDetectionComputation(graphDatabase, NODE_PREFERENCE, HOP_ATTENUATION, MAX_ITERATIONS).run();
-		// Verify output
-		List<List<Long>> expectedOutput = parseOutputResource("/test-examples/cd-output");
-		try (Transaction transaction = graphDatabase.beginTx()) {
-			for (List<Long> community : expectedOutput) {
-				long communityHead = community.get(0);
-				long communityId = (long)getNode(communityHead).getProperty(LABEL, Long.MAX_VALUE);
-
-				for (long communityMember : community) {
-					long communityIdOfMember = (long)getNode(communityMember).getProperty(LABEL, Long.MIN_VALUE);
-					Assert.assertThat("expected vertices " + communityHead + " and " + communityMember +
-							" to be in the same community",	communityIdOfMember, is(communityId));
-				}
-			}
-		}
-	}
 
 	private static List<List<Long>> parseOutputResource(String resourceName) throws IOException {
 		try (BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(
@@ -61,6 +43,32 @@ public class CommunityDetectionComputationTest extends AbstractComputationTest {
 				communities.add(community);
 			}
 			return communities;
+		}
+	}
+
+	@Test
+	public void testExample() throws IOException {
+		// Load data
+		loadGraphFromResource("/test-examples/cd-input");
+		// Execute algorithm
+		new CommunityDetectionComputation(graphDatabase, NODE_PREFERENCE, HOP_ATTENUATION, MAX_ITERATIONS).run();
+		// Verify output
+		List<List<Long>> expectedOutput = parseOutputResource("/test-examples/cd-output");
+		try (Transaction transaction = graphDatabase.beginTx()) {
+			Set<Long> communityIds = new HashSet<>();
+			for (List<Long> community : expectedOutput) {
+				long communityHead = community.get(0);
+				long communityId = (long) getNode(communityHead).getProperty(LABEL, Long.MAX_VALUE);
+				assertThat("expected " + communityHead + " to be in a new community",
+						communityId, not(isIn(communityIds)));
+				communityIds.add(communityId);
+
+				for (long communityMember : community) {
+					long communityIdOfMember = (long) getNode(communityMember).getProperty(LABEL, Long.MIN_VALUE);
+					assertThat("expected vertices " + communityHead + " and " + communityMember +
+							" to be in the same community", communityIdOfMember, is(communityId));
+				}
+			}
 		}
 	}
 
