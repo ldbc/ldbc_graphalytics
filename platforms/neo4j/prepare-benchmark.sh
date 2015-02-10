f24f371ce5d02066549f95e87fa654e110a2fe92
Tim Hegeman <tim.m.hegeman@gmail.com>
2015-02-10 18:07:10 +0100

    Finished neo4j configuration

--- platforms/neo4j/prepare-benchmark.sh
+++ platforms/neo4j/prepare-benchmark.sh
@@ -1,2 +1,18 @@
 #!/bin/sh
 
+# Ensure the configuration file exists
+if [ ! -f "$config/neo4j.properties" ]; then
+	echo "Missing mandatory configuration file: $config/neo4j.properties" >&2
+	exit 1
+fi
+
+# Get the first specification of jvm.heap.size.mb
+heapsize=$(grep -E "^jvm\.heap\.size\.mb[	 ]*[:=]" $config/neo4j.properties | sed 's/jvm\.heap\.size\.mb[\t ]*[:=][\t ]*\([^\t ]*\).*/\1/g' | head -n 1)
+if [ "h$heapsize" = "h" ]; then
+	echo "Invalid definition of jvm.heap.size.mb: $heapsize" >&2
+	exit 1
+fi
+echo "Using heap size: $heapsize"
+
+export platform_opts="-Xmx${heapsize}m -Xms${heapsize}m"
+
