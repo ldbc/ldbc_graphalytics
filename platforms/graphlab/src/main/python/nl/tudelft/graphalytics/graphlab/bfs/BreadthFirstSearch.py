e7dfb2458369db750de7d42cad6d4af591c00bfb
Erackron <jorairijsdijk@gmail.com>
2015-02-11 23:59:39 +0100

    Change BFS and CONN to use local execution until GraphLab gets distributed execution.
    The code still uses the exact structure to make sure transitioning back to deployed execution is easy in the future

--- platforms/graphlab/src/main/python/nl/tudelft/graphalytics/graphlab/bfs/BreadthFirstSearch.py
+++ platforms/graphlab/src/main/python/nl/tudelft/graphalytics/graphlab/bfs/BreadthFirstSearch.py
@@ -1,6 +1,5 @@
 import sys
 import os
-import time
 
 import graphlab as gl
 from graphlab.deploy.environment import Hadoop
@@ -32,11 +31,12 @@ heap_size = sys.argv[2]
 graph_file = sys.argv[3]
 directed = sys.argv[4] == "true"
 edge_based = sys.argv[5] == "true"
-source_vertex = sys.argv[6]
+source_vertex = long(sys.argv[6])
 
 # Create hadoop environment object
 hadoop_home = os.environ.get('HADOOP_HOME')
-hadoop = create_environment(hadoop_home=hadoop_home, memory_mb=heap_size, virtual_cores=virtual_cores)
+# TODO Switch back to using hadoop deployment when distributed execution is possible.
+# hadoop = create_environment(hadoop_home=hadoop_home, memory_mb=heap_size, virtual_cores=virtual_cores)
 
 if not edge_based:
     print >> sys.stderr, "Vertex based graph format not supported yet"
@@ -50,7 +50,7 @@ if not directed:
 def load_graph_task(task):
     import graphlab as gl_
 
-    graph_data = gl_.SFrame.read_csv(task.params['csv'], header=False, delimiter=' ')
+    graph_data = gl_.SFrame.read_csv(task.params['csv'], header=False, delimiter=' ', column_type_hints=long)
     task.outputs['graph'] = gl.SGraph().add_edges(graph_data, src_field='X1', dst_field='X2')
 
 
@@ -62,21 +62,39 @@ def shortest_path_model(task):
 
 
 # Define the graph loading task
-load_graph = gl.deploy.Task('load_graph')
-load_graph.set_params({'csv': graph_file})
-load_graph.set_code(load_graph_task)
-load_graph.set_outputs(['graph'])
+# TODO Switch back to using code hadoop deployment when distributed execution is possible. Note: files are still located on the HDFS.
+# load_graph = gl.deploy.Task('load_graph')
+# load_graph.set_params({'csv': graph_file})
+# load_graph.set_code(load_graph_task)
+# load_graph.set_outputs(['graph'])
+#
+# # Define the shortest_path model create task
+# shortest = gl.deploy.Task('shortest_path')
+# shortest.set_params({'source_vertex': source_vertex})
+# shortest.set_inputs({'data': ('load_graph', 'graph')})
+# shortest.set_code(shortest_path_model)
+# shortest.set_outputs(['sp_graph'])
+#
+# # Create the job and deploy it to the Hadoop cluster
+# hadoop_job = gl.deploy.job.create(['load_graph', 'shortest_path'], environment=hadoop)
+# while hadoop_job.get_status() in ['Pending', 'Running']:
+#     time.sleep(2)  # sleep for 2s while polling for job to be complete.
+#
+# print hadoop_job
 
-# Define the shortest_path model create task
-shortest = gl.deploy.Task('shortest_path')
-shortest.set_params({'source_vertex': source_vertex})
-shortest.set_inputs({'data': ('load_graph', 'graph')})
-shortest.set_code(shortest_path_model)
-shortest.set_outputs(['sp_graph'])
 
-# Create the job and deploy it to the Hadoop cluster
-hadoop_job = gl.deploy.job.create(['load_graph', 'shortest_path'], environment=hadoop)
-while hadoop_job.get_status() in ['Pending', 'Running']:
-    time.sleep(2)  # sleep for 2s while polling for job to be complete.
+# Local implementation as distributed is not yet possible, so hadoop is only an extra amount of overhead
+# Remove everything from these comments on when switching back to deployment execution
 
-print hadoop_job
+# Stub task class
+class Task:
+    def __init__(self, **keywords):
+        self.__dict__.update(keywords)
+
+# Stub task object to keep function definitions intact
+cur_task = Task(params={'csv': graph_file, 'source_vertex': source_vertex}, inputs={}, outputs={})
+
+load_graph_task(cur_task)
+cur_task.inputs['data'] = cur_task.outputs['graph']
+shortest_path_model(cur_task)
+output_graph = cur_task.outputs['sp_graph']
--- platforms/graphlab/src/main/python/nl/tudelft/graphalytics/graphlab/conn/ConnectedComponents.py
+++ platforms/graphlab/src/main/python/nl/tudelft/graphalytics/graphlab/conn/ConnectedComponents.py
@@ -1,9 +1,9 @@
 import sys
 import os
-import time
 
 import graphlab as gl
 from graphlab.deploy.environment import Hadoop
+
 
 __author__ = 'Jorai Rijsdijk'
 
@@ -34,7 +34,8 @@ edge_based = sys.argv[5] == "true"
 
 # Create hadoop environment object
 hadoop_home = os.environ.get('HADOOP_HOME')
-hadoop = create_environment(hadoop_home=hadoop_home, memory_mb=heap_size, virtual_cores=virtual_cores)
+# TODO Switch back to using hadoop deployment when distributed execution is possible.
+# hadoop = create_environment(hadoop_home=hadoop_home, memory_mb=heap_size, virtual_cores=virtual_cores)
 
 if not edge_based:
     print >> sys.stderr, "Vertex based graph format not supported yet"
@@ -44,7 +45,7 @@ if not edge_based:
 def load_graph_task(task):
     import graphlab as gl_
 
-    graph_data = gl_.SFrame.read_csv(task.params['csv'], header=False, delimiter=' ')
+    graph_data = gl_.SFrame.read_csv(task.params['csv'], header=False, delimiter=' ', column_type_hints=long)
     task.outputs['graph'] = gl.SGraph().add_edges(graph_data, src_field='X1', dst_field='X2')
 
 
@@ -52,24 +53,43 @@ def connected_components_model(task):
     import graphlab as gl_
 
     graph = task.inputs['data']
-    task.outputs['sp_graph'] = gl_.connected_components.create(graph)
+    task.outputs['cc_graph'] = gl_.connected_components.create(graph)
 
 
-# Define the graph loading task
-load_graph = gl.deploy.Task('load_graph')
-load_graph.set_params({'csv': graph_file})
-load_graph.set_code(load_graph_task)
-load_graph.set_outputs(['graph'])
+# # Define the graph loading task
+# TODO Switch back to using hadoop deployment when distributed execution is possible. Note: files are still located on the HDFS.
+# load_graph = gl.deploy.Task('load_graph')
+# load_graph.set_params({'csv': graph_file})
+# load_graph.set_code(load_graph_task)
+# load_graph.set_outputs(['graph'])
+#
+# # Define the shortest_path model create task
+# connected = gl.deploy.Task('connected_components')
+# connected.set_inputs({'data': ('load_graph', 'graph')})
+# connected.set_code(connected_components_model)
+# connected.set_outputs(['cc_graph'])
+#
+# # Create the job and deploy it to the Hadoop cluster
+# hadoop_job = gl.deploy.job.create(['load_graph', 'connected_components'], environment=hadoop)
+# while hadoop_job.get_status() in ['Pending', 'Running']:
+#     time.sleep(2)  # sleep for 2s while polling for job to be completed.
+#
+# print hadoop_job
 
-# Define the shortest_path model create task
-connected = gl.deploy.Task('connected_components')
-connected.set_inputs({'data': ('load_graph', 'graph')})
-connected.set_code(connected_components_model)
-connected.set_outputs(['sp_graph'])
 
-# Create the job and deploy it to the Hadoop cluster
-hadoop_job = gl.deploy.job.create(['load_graph', 'connected_components'], environment=hadoop)
-while hadoop_job.get_status() in ['Pending', 'Running']:
-    time.sleep(2)  # sleep for 2s while polling for job to be completed.
+# Local implementation as distributed is not yet possible, so hadoop is only an extra amount of overhead
+# Remove everything from these comments on when switching back to deployment execution
 
-print hadoop_job
+# Stub task class
+class Task:
+    def __init__(self, **keywords):
+        self.__dict__.update(keywords)
+
+# Stub task object to keep function definitions intact
+cur_task = Task(params={'csv': graph_file}, inputs={}, outputs={})
+
+load_graph_task(cur_task)
+cur_task.inputs['data'] = cur_task.outputs['graph']
+connected_components_model(cur_task)
+output_graph = cur_task.outputs['cc_graph']
+
