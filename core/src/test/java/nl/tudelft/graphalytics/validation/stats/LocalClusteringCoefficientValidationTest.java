634fc9253bc09a2783c1402d18e62e8229199b15
Tim Hegeman <tim.m.hegeman@gmail.com>
2015-03-28 21:32:11 +0100

    Added javadoc to LCC test

--- core/src/test/java/nl/tudelft/graphalytics/validation/stats/LocalClusteringCoefficientValidationTest.java
+++ core/src/test/java/nl/tudelft/graphalytics/validation/stats/LocalClusteringCoefficientValidationTest.java
@@ -13,41 +13,37 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
 
 /**
+ * Framework for validating the output of an implementation of the local clustering coefficient algorithm. Defines two
+ * functions to be implemented to run a platform-specific local clustering coefficient implementation on an in-memory
+ * graph.
+ *
  * @author Tim Hegeman
  */
 public abstract class LocalClusteringCoefficientValidationTest {
 
-	private void validateLocalClusteringCoefficient(LocalClusteringCoefficientOutput executionResult,
-			String graphOutputPath, String lccOutputPath) throws IOException {
-		final double EPSILON = 1e-6;
-
-		GraphValues<Double> outputGraph = GraphParser.parseGraphValuesFromDataset(
-				getClass().getResourceAsStream(graphOutputPath), new DoubleParser());
-		double expectedMeanLcc = loadExpectedMeanLocalClusteringCoefficient(lccOutputPath);
-
-		assertThat("result graph has the correct number of vertices",
-				executionResult.getVertices(), hasSize(outputGraph.getVertices().size()));
-		assertThat("result graph has the expected vertex ids",
-				executionResult.getVertices(), containsInAnyOrder(outputGraph.getVertices().toArray()));
-		for (long vertexId : outputGraph.getVertices()) {
-			assertThat("vertex " + vertexId + " has correct value",
-					executionResult.getComponentIdForVertex(vertexId),
-					is(closeTo(outputGraph.getVertexValue(vertexId), EPSILON)));
-		}
-
-		assertThat("mean local clustering coefficient is correct",
-				executionResult.getMeanLocalClusteringCoefficient(), is(closeTo(expectedMeanLcc, EPSILON)));
-	}
-
-	private double loadExpectedMeanLocalClusteringCoefficient(String lccOutputPath) throws IOException {
-		try (Scanner scanner = new Scanner(getClass().getResourceAsStream(lccOutputPath))) {
-			return scanner.nextDouble();
-		}
-	}
-
+	/**
+	 * Executes the platform-specific implementation of the local clustering coefficient algorithm on an in-memory
+	 * directed graph, and returns the output of the execution.
+	 * <p/>
+	 * This function is called with sample graphs and the output is compared with known-correct results.
+	 *
+	 * @param graph the graph to execute the local clustering coefficient algorithm on
+	 * @return the output of the local clustering coefficient algorithm
+	 * @throws Exception
+	 */
 	public abstract LocalClusteringCoefficientOutput executeDirectedLocalClusteringCoefficient(
 			GraphStructure graph) throws Exception;
 
+	/**
+	 * Executes the platform-specific implementation of the local clustering coefficient algorithm on an in-memory
+	 * undirected graph, and returns the output of the execution.
+	 * <p/>
+	 * This function is called with sample graphs and the output is compared with known-correct results.
+	 *
+	 * @param graph the graph to execute the local clustering coefficient algorithm on
+	 * @return the output of the local clustering coefficient algorithm
+	 * @throws Exception
+	 */
 	public abstract LocalClusteringCoefficientOutput executeUndirectedLocalClusteringCoefficient(
 			GraphStructure graph) throws Exception;
 
@@ -77,6 +73,50 @@ public abstract class LocalClusteringCoefficientValidationTest {
 		LocalClusteringCoefficientOutput executionResult = executeUndirectedLocalClusteringCoefficient(inputGraph);
 
 		validateLocalClusteringCoefficient(executionResult, graphOutputPath, lccOutputPath);
+	}
+
+	/**
+	 * Validates the output of a local clustering coefficient implementation. The output is compared with known results
+	 * in separate files.
+	 *
+	 * @param executionResult the result of the breadth-first search execution
+	 * @param graphOutputPath the output file to read the correct results from
+	 * @param lccOutputPath   the file to read the correct mean local clustering coefficient from
+	 * @throws IOException iff the output file could not be loaded
+	 */
+	private void validateLocalClusteringCoefficient(LocalClusteringCoefficientOutput executionResult,
+	                                                String graphOutputPath, String lccOutputPath) throws IOException {
+		final double EPSILON = 1e-6;
+
+		GraphValues<Double> outputGraph = GraphParser.parseGraphValuesFromDataset(
+				getClass().getResourceAsStream(graphOutputPath), new DoubleParser());
+		double expectedMeanLcc = loadExpectedMeanLocalClusteringCoefficient(lccOutputPath);
+
+		assertThat("result graph has the correct number of vertices",
+				executionResult.getVertices(), hasSize(outputGraph.getVertices().size()));
+		assertThat("result graph has the expected vertex ids",
+				executionResult.getVertices(), containsInAnyOrder(outputGraph.getVertices().toArray()));
+		for (long vertexId : outputGraph.getVertices()) {
+			assertThat("vertex " + vertexId + " has correct value",
+					executionResult.getLocalClusteringCoefficientForVertex(vertexId),
+					is(closeTo(outputGraph.getVertexValue(vertexId), EPSILON)));
+		}
+
+		assertThat("mean local clustering coefficient is correct",
+				executionResult.getMeanLocalClusteringCoefficient(), is(closeTo(expectedMeanLcc, EPSILON)));
+	}
+
+	/**
+	 * Reads the expected mean local clustering coefficient from a file.
+	 *
+	 * @param lccOutputPath the path of the file containing the mean local clustering coefficient
+	 * @return the read mean local clustering coefficient
+	 * @throws IOException iff the file could not be read
+	 */
+	private double loadExpectedMeanLocalClusteringCoefficient(String lccOutputPath) throws IOException {
+		try (Scanner scanner = new Scanner(getClass().getResourceAsStream(lccOutputPath))) {
+			return scanner.nextDouble();
+		}
 	}
 
 }
