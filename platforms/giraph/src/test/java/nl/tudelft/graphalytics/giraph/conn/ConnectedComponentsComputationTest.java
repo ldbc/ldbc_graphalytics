224675a6b5f596dcb66822b360dad25edecf3735
Tim Hegeman <tim.m.hegeman@gmail.com>
2015-03-28 14:07:01 +0100

    Reimplemented Giraph CC test to use core test

--- platforms/giraph/src/test/java/nl/tudelft/graphalytics/giraph/conn/ConnectedComponentsComputationTest.java
+++ platforms/giraph/src/test/java/nl/tudelft/graphalytics/giraph/conn/ConnectedComponentsComputationTest.java
@@ -1,67 +1,53 @@
 package nl.tudelft.graphalytics.giraph.conn;
 
-import nl.tudelft.graphalytics.giraph.AbstractComputationTest;
+import nl.tudelft.graphalytics.giraph.GiraphTestGraphLoader;
+import nl.tudelft.graphalytics.validation.GraphStructure;
+import nl.tudelft.graphalytics.validation.conn.ConnectedComponentsOutput;
+import nl.tudelft.graphalytics.validation.conn.ConnectedComponentsValidationTest;
 import org.apache.giraph.conf.GiraphConfiguration;
 import org.apache.giraph.graph.Computation;
+import org.apache.giraph.graph.Vertex;
+import org.apache.giraph.utils.InternalVertexRunner;
 import org.apache.giraph.utils.TestGraph;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.NullWritable;
-import org.junit.Test;
 
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.*;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
- * Test class for the connected components algorithm. Executes the Giraph implementation of the CC computation on a
- * small graph, and verifies that the output of the computation matches the expected results.
- *
  * @author Tim Hegeman
  */
-public class ConnectedComponentsComputationTest extends AbstractComputationTest<LongWritable, NullWritable> {
+public class ConnectedComponentsComputationTest extends ConnectedComponentsValidationTest {
 
-	@Test
-	public void testDirectedExample() throws Exception {
-		performTest(DirectedConnectedComponentsComputation.class,
-				"/test-examples/conn-dir-input",
-				"/test-examples/conn-dir-output");
-	}
-
-	@Test
-	public void testUndirectedExample() throws Exception {
-		performTest(UndirectedConnectedComponentsComputation.class,
-				"/test-examples/conn-undir-input",
-				"/test-examples/conn-undir-output");
-	}
-
-	private void performTest(Class<? extends Computation> computationClass, String input, String output)
-			throws Exception {
+	private static ConnectedComponentsOutput executeConnectedComponents(Class<? extends Computation> computationClass,
+	                                                                    GraphStructure graph) throws Exception {
 		GiraphConfiguration configuration = new GiraphConfiguration();
 		configuration.setComputationClass(computationClass);
 
+		TestGraph<LongWritable, LongWritable, NullWritable> inputGraph =
+				GiraphTestGraphLoader.createGraph(configuration, graph, new LongWritable(-1), NullWritable.get());
+
 		TestGraph<LongWritable, LongWritable, NullWritable> result =
-				runTest(configuration, input);
-		TestGraph<LongWritable, LongWritable, NullWritable> expected =
-				parseGraphValues(configuration, output);
+				InternalVertexRunner.runWithInMemoryOutput(configuration, inputGraph);
 
-		assertThat("result graph has the correct number of vertices",
-				result.getVertices().keySet(), hasSize(expected.getVertices().size()));
-		for (LongWritable vertexId : result.getVertices().keySet())
-			assertThat("vertex " + vertexId + " has correct value",
-					result.getVertex(vertexId).getValue(), is(equalTo(expected.getVertex(vertexId).getValue())));
+		Map<Long, Long> pathLengths = new HashMap<>();
+		for (Map.Entry<LongWritable, Vertex<LongWritable, LongWritable, NullWritable>> vertexEntry :
+				result.getVertices().entrySet()) {
+			pathLengths.put(vertexEntry.getKey().get(), result.getVertex(vertexEntry.getKey()).getValue().get());
+		}
+
+		return new ConnectedComponentsOutput(pathLengths);
 	}
 
 	@Override
-	protected LongWritable getDefaultValue(long vertexId) {
-		return new LongWritable(vertexId);
+	public ConnectedComponentsOutput executeDirectedConnectedComponents(GraphStructure graph) throws Exception {
+		return executeConnectedComponents(DirectedConnectedComponentsComputation.class, graph);
 	}
 
 	@Override
-	protected NullWritable getDefaultEdgeValue(long sourceId, long destinationId) {
-		return NullWritable.get();
+	public ConnectedComponentsOutput executeUndirectedConnectedComponents(GraphStructure graph) throws Exception {
+		return executeConnectedComponents(UndirectedConnectedComponentsComputation.class, graph);
 	}
 
-	@Override
-	protected LongWritable parseValue(long vertexId, String value) {
-		return new LongWritable(Long.parseLong(value));
-	}
 }
