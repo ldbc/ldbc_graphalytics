a01004b4cbbc77122aec5e2d8a547a3376c6b264
Tim Hegeman <tim.m.hegeman@gmail.com>
2015-03-28 21:35:36 +0100

    Added javadoc to graph structures for validation tests

--- core/src/test/java/nl/tudelft/graphalytics/validation/GraphStructure.java
+++ core/src/test/java/nl/tudelft/graphalytics/validation/GraphStructure.java
@@ -13,16 +13,26 @@ public final class GraphStructure {
 
 	private final Map<Long, Set<Long>> edgeLists;
 
+	/**
+	 * @param edgeLists a map containing a set of outgoing edges for each vertex
+	 */
 	public GraphStructure(Map<Long, Set<Long>> edgeLists) {
 		this.edgeLists = new HashMap<>(edgeLists);
 	}
 
-	public final Set<Long> getEdgesForVertex(long vertexId) {
-		return edgeLists.get(vertexId);
-	}
-
+	/**
+	 * @return a set of vertex ids in the graph
+	 */
 	public final Set<Long> getVertices() {
 		return edgeLists.keySet();
+	}
+
+	/**
+	 * @param vertexId the id of a vertex in the graph
+	 * @return the corresponding set of outgoing edges
+	 */
+	public final Set<Long> getEdgesForVertex(long vertexId) {
+		return edgeLists.get(vertexId);
 	}
 
 }
--- core/src/test/java/nl/tudelft/graphalytics/validation/GraphValues.java
+++ core/src/test/java/nl/tudelft/graphalytics/validation/GraphValues.java
@@ -13,14 +13,24 @@ public class GraphValues<ValueType> {
 
 	private final Map<Long, ValueType> vertexValues;
 
+	/**
+	 * @param vertexValues a map containing a vertex value for each vertex
+	 */
 	public GraphValues(Map<Long, ValueType> vertexValues) {
 		this.vertexValues = new HashMap<>(vertexValues);
 	}
 
+	/**
+	 * @return a set of vertex ids in the graph
+	 */
 	public Set<Long> getVertices() {
 		return vertexValues.keySet();
 	}
 
+	/**
+	 * @param vertexId the id of a vertex in the graph
+	 * @return the corresponding vertex value
+	 */
 	public ValueType getVertexValue(long vertexId) {
 		return vertexValues.get(vertexId);
 	}
