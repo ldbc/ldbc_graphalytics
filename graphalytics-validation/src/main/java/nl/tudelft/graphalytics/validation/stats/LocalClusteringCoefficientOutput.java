6f6a0b3e64e24022518d34e8050a3370cceec5c9
Tim Hegeman <tim.m.hegeman@gmail.com>
2016-01-18 13:56:21 +0100

    Remove mean LCC from test cases

--- graphalytics-validation/src/main/java/nl/tudelft/graphalytics/validation/stats/LocalClusteringCoefficientOutput.java
+++ graphalytics-validation/src/main/java/nl/tudelft/graphalytics/validation/stats/LocalClusteringCoefficientOutput.java
@@ -28,16 +28,12 @@ import java.util.Set;
 public class LocalClusteringCoefficientOutput {
 
 	private final Map<Long, Double> localClusteringCoefficients;
-	private final double meanLocalClusteringCoefficient;
 
 	/**
 	 * @param localClusteringCoefficients    a map containing the local clustering coefficient of each vertex
-	 * @param meanLocalClusteringCoefficient the mean local clustering coefficient
 	 */
-	public LocalClusteringCoefficientOutput(Map<Long, Double> localClusteringCoefficients,
-	                                        double meanLocalClusteringCoefficient) {
+	public LocalClusteringCoefficientOutput(Map<Long, Double> localClusteringCoefficients) {
 		this.localClusteringCoefficients = new HashMap<>(localClusteringCoefficients);
-		this.meanLocalClusteringCoefficient = meanLocalClusteringCoefficient;
 	}
 
 	/**
@@ -53,13 +49,6 @@ public class LocalClusteringCoefficientOutput {
 	 */
 	public double getLocalClusteringCoefficientForVertex(long vertexId) {
 		return localClusteringCoefficients.get(vertexId);
-	}
-
-	/**
-	 * @return the mean local clustering coefficient
-	 */
-	public double getMeanLocalClusteringCoefficient() {
-		return meanLocalClusteringCoefficient;
 	}
 
 }
