0c6463be35ca01b2a31d8d7612f18a0394d5de6c
Tim Hegeman <tim.m.hegeman@gmail.com>
2016-05-13 11:21:21 +0200

    Remove unused import

--- graphalytics-core/src/main/java/nl/tudelft/graphalytics/validation/VertexValidator.java
+++ graphalytics-core/src/main/java/nl/tudelft/graphalytics/validation/VertexValidator.java
@@ -16,7 +16,6 @@
 package nl.tudelft.graphalytics.validation;
 
 import java.io.BufferedReader;
-import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.nio.file.*;
