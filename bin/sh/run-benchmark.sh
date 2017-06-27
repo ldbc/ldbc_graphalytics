1e4f2d44ddcdb95f49e75a2bd623746088ae9b82
Wing Lung Ngai <w.l.ngai@vu.nl>
2017-06-27 17:21:23 +0200

    Refactor run-benchmark scripts.

--- bin/sh/run-benchmark.sh
+++ bin/sh/run-benchmark.sh
@@ -55,21 +55,13 @@ done
 export config=$config
 . ${rootdir}/bin/sh/prepare-benchmark.sh "$@"
 
-# Verify that corresponding binary exists
-if ! find lib -name "graphalytics-platforms-$platform*.jar" | grep -q '.'; then
-	echo "No binary exist in lib/ for platform \"$platform\"" >&2
-	exit 1
-fi
-
 # Verify that the library jar is set
 if [ "$LIBRARY_JAR" = "" ]; then
 	echo "The prepare-benchmark.sh script must set variable \$LIBRARY_JAR" >&2
-	echo "Fall back to support legacy library jar" >&2
-	LIBRARY_JAR=$(find ${rootdir}/lib/graphalytics-platforms-$platform*.jar)
-	#exit 1
+	exit 1
 fi
 
 # Run the benchmark
 export CLASSPATH=$config:$(find ${rootdir}/$LIBRARY_JAR):$platform_classpath
-java -cp $CLASSPATH $java_opts science.atlarge.graphalytics.BenchmarkSuite $platform_opts
+java -cp $CLASSPATH $java_opts science.atlarge.graphalytics.BenchmarkSuite
 
