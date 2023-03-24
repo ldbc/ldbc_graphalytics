197366022730ece8139509c443e7bb00f1e2094c
Gabor Szarnyas <szarnyasg@gmail.com>
2023-03-24 15:16:32 +0100

    Revert "Print DEBUG-level log information to stdout for easier troubleshooting"
    This reverts commit 71c93a35841948ba9dd82917a0b51a6f9612a87b.

--- graphalytics-core/src/main/java/science/atlarge/graphalytics/BenchmarkSuite.java
+++ graphalytics-core/src/main/java/science/atlarge/graphalytics/BenchmarkSuite.java
@@ -49,7 +49,7 @@ public class BenchmarkSuite {
 	public static void main(String[] args) throws IOException {
 
 		LogUtil.intializeLoggers();
-		LogUtil.appendConsoleLogger(Level.DEBUG);
+		LogUtil.appendConsoleLogger(Level.INFO);
 		LOG = LogManager.getLogger();
 
 
@@ -76,7 +76,7 @@ public class BenchmarkSuite {
 		}
 
 		LOG.info(String.format("Executing Benchmark..."));
-		LogUtil.appendFileLogger(Level.DEBUG, "file-reduced", benchmark.getBaseReportDir().resolve("log/benchmark-summary.log"));
+		LogUtil.appendFileLogger(Level.INFO, "file-reduced", benchmark.getBaseReportDir().resolve("log/benchmark-summary.log"));
 		LogUtil.appendFileLogger(Level.TRACE, "file-full", benchmark.getBaseReportDir().resolve("log/benchmark-full.log"));
 		ConsoleUtil.displayTrademark(platform.getPlatformName());
 
